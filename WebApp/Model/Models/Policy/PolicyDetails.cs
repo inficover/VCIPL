05ef3dcc7d243937837c4b6e6ff1ab5612e9f613
manasa <manasa.inficover@gmail.com>
2020-05-31 21:43:51 +0530

    Policy- AddedModels

---
+++ WebApp/Model/Models/Policy/PolicyDetails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Models.Policy
+{
+    public class PolicyDetails
+    {
+        public int Id { get; set; }
+        public string VehicleType { get; set; }
+        public string PolicyType { get; set; }
+        public DateTime? PolicyIssuenceDate { get; set; }
+        public string RegistrationNo { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Variant { get; set; }
+        public string FuelType { get; set; }
+        public decimal AddOnPremium { get; set; }
+        public string InsuredName { get; set; }
+        public string InsuredMobile { get; set; }
+        public string Insurer { get; set; }
+        public string PaymentMode { get; set; }
+        public string PaymentModeOthers { get; set; }
+        public decimal ODPremium { get; set; }
+        public decimal NetPremium { get; set; }
+        public decimal GrossPremium { get; set; }
+        public string Broker { get; set; }
+        public string Status { get; set; }
+        public string CreatedBy { get; set; }
+
+    }
+}
