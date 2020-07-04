85f442eb5b0249c5bfc2c2f0371c35d7609713cb
manasa <manasa.inficover@gmail.com>
2020-07-04 09:31:52 +0530

    Added Get Vehicles SP

---
+++ Database/StoredProcedures/GetVehiclesByCriteria.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[GetVehiclesByCriteria]
+	@MakesList IntegersList readonly,
+	@ModelsList IntegersList readonly,
+	@VarientsList IntegersList readonly
+AS
+BEGIN
+	declare @makesCount int
+	declare @modelsCount int
+	declare @varientsCount int
+
+	select @makesCount = count(*) from @MakesList
+	select @varientsCount = count(*) from @ModelsList
+	select @modelsCount = count(*) from @VarientsList
+
+	select m.name, md.name, v.name from makes m 
+	inner join Models md on md.MakeId = m.Id
+	inner join Variants v on v.ModelId = md.Id
+
+	where 1 = 1
+	AND (@makesCount = 0 or m.Id in (select id from @MakesList))
+	AND (@varientsCount = 0 or md.Id in (select id from @ModelsList))
+	AND (@modelsCount = 0 or v.Id in (select id from @VarientsList))
+END
+
+
+
+--DECLARE @mkList IntegersList;
+--DECLARE @mdList IntegersList;
+--DECLARE @VList IntegersList;
+  
+--INSERT INTO @mkList VALUES (1),(2);
+--INSERT INTO @mdList VALUES (1), (9);
+
+--exec [GetVehiclesByCriteria] @mkList, @mdList, @VList
+
+--Select * from Makes
+--Select * from Models
+--Select * from Variants
